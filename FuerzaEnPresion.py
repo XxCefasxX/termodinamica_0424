268ab1a5ad1cac7f44087e35731db4eddbed2d57
Pedro Salazar <salazar9_5@hotmail.com>
2024-10-30 19:21:32 -0700

    funciones

--- FuerzaEnPresion.py
+++ FuerzaEnPresion.py
@@ -1,20 +1,22 @@
-#Formulas
-#1----- Pabs ​= Pman ​+ Patm​
-#2----- Ftotal=P*A
-#Requisitos
-#Ftotal
-#P
-#A
+#"Un tanque de gas tiene una presión manométrica de 2×10⁵ Pa. La presión atmosférica es 101,325Pa. 
+# El tanque está en contacto con una superficie cuya área es 0.5 m². 
+# ¿Cuál es la presión absoluta dentro del tanque? 
+# ¿Cuál es la fuerza total ejercida sobre la superficie? "
 
-#1 Pedir datos
+from presion import *
+
+#Pedir datos
 Pman= float( input("Ingresa la presion manometrica en Pascales"))
 Patm= float( input("Ingresa la presion atmosferica en Pascales"))
-A= float(input("Ingresa el area "))
-#2 APlicar formula
-Pabs = Pman + Patm#calcula la Presion absoluta
-Ftotal=Pabs*A #Calcula la furza total
-#3 Formatear decimales
-Fuerza = round(Ftotal,2)
-Presion = round(Pabs,2)
-#4 Mostrar resultado
-print(f"La presión absoluta es {Presion}Pa y la fuerza sobre la superficie es {Fuerza}N ")
+Area= float(input("Ingresa el area "))
+
+#llamr funciones
+Pabs = PresionAbsoluta(Pman,Patm)
+Ftotal=FuerzaTotal(Pabs,Area)
+
+#Formatear decimales
+F = round(Ftotal,2)
+P = round(Pabs,2)
+
+#Mostrar resultado
+print(f"La presión absoluta es {P}Pa y la fuerza sobre la superficie es {F}N ")
--- presion.py
+++ presion.py
@@ -1,22 +1,12 @@
-#formula
-#P=Ftotal/A
-#P
-#Ftotal
-#A
 
-#Pedir datos
-Ft=int (input("Ingresa la fuerza total "))
-A= int( input("Ingresa el area de la base "))
+def Presion(FuerzaTotal,Area):
+    Presion=FuerzaTotal/Area,2
+    return Presion
 
-#APlicar formula
-P=Ft/A
-#formatear a 2 decimales
-result =round(P,2)
+def FuerzaTotal(Pabs,Area):
+    Ftotal=Pabs*Area 
+    return Ftotal
 
-#Validar si es mas de 1000 Pa
-if(result>1000):
-    #Convierte a Kilo Pascales
-    print(f"La presion es: {result/1000}KPa")
-else:
-    #Muestra el resultado en pascales
-    print(f"La presion es: {result}Pa")
+def PresionAbsoluta(Pman,Patm):
+    Pabs = Pman + Patm
+    return Pabs
